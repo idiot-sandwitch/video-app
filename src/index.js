33ede68aa3d069de7dc2ee12fa08c924f7d4dc06
Ayushman <51037059+Dark0017@users.noreply.github.com>
2021-04-28 09:15:06 +0530

    wrote socket io connection code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const path = require("path");
+const socketio = require("socket.io");
+const http = require("http");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on("connection", (socket) => {
+  console.log("New Connection");
+
+  //on join the chat
+  
+
+});
+
 const pathToPublic = path.join(__dirname, "../public");
 
 const port = process.env.PORT || 3000;
 
 app.use(express.static(pathToPublic));
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log(`Listening to ${port}`);
 });
