79e812ba349cff7bf948d91aa05261917cbc16ee
Avijit Pandey <avijeetpandey87@gmail.com>
2021-04-30 04:10:06 +0530

    added stun server based webrtc p2p connection for a room with 2 people

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,138 @@
 socket = io();
+let root = document.getElementById("video-chat-lobby");
+let divVideoChat = document.getElementById("video-chat-room");
+let joinButton = document.getElementById("join-video");
+const userFeed = document.getElementById("user-video");
+const peerFeed = document.getElementById("peer-video");
+let roomInput = document.getElementById("roomName");
+let rtcPeerConnection;
+let userStream;
+
+//Ice Server Configuration, currently includes only stun servers.
+let iceServers = {
+  iceServers: [
+    { urls: "stun:stun.ekiga.net:3478" },
+    { urls: "stun:stun.l.google.com:19302" },
+  ],
+};
+
+//initializes user media and broadcasts ready for p2p status if the second user has joined.
+let isOwner = false;
+socket.on("room-joined", async (isNew) => {
+  //DEBUG LOG: console.log(`room-joined request caught by client ${socket.id}`);
+  isOwner = isNew;
+  const constraints = {};
+  const cameras = await listMediaDevice("videoinput");
+  const mics = await listMediaDevice("audioinput");
+
+  constraints.video = cameras.length !== 0 ? true : false;
+  constraints.audio = mics.length !== 0 ? true : false;
+
+  //Check for constraint video and audio true or not
+  if (constraints.video === false)
+    return displayError("Video Device doesn't exist");
+  if (constraints.audio === false)
+    return displayError("Audio Device doesn't exist");
+
+  //check user media for audio false
+  if (!(await checkUserMedia("video"))) {
+    displayError("Video Feed is broken");
+    constraints.video = false;
+    return;
+  }
+  //check user media for video false
+  if (!(await checkUserMedia("audio"))) {
+    displayError("Audio Feed is broken");
+    constraints.audio = false;
+    return;
+  }
+  //set stream to user media output
+
+  root.style = "display:none";
+  userStream = await navigator.mediaDevices.getUserMedia(constraints);
+  userFeed.srcObject = userStream;
+  userFeed.onloadedmetadata = () => userFeed.play();
+  if (!isOwner) socket.emit("ready", roomInput.value);
+});
+
+socket.on("room-full", () => {
+  displayError("cannot join room as it is full!");
+});
+
+socket.on("ready", () => {
+  //DEBUG LOG: console.log(
+  //   `client ${socket.id} recieved forwarded ready request from server.`
+  // );
+  if (isOwner) {
+    rtcpConnection = new RTCPeerConnection(iceServers);
+    rtcpConnection.onicecandidate = (e) => {
+      if (e.candidate) socket.emit("candidate", e.candidate, roomInput.value);
+    };
+    rtcpConnection.ontrack = onTrackFn;
+    rtcpConnection.addTrack(userStream.getTracks()[0], userStream);
+    rtcpConnection.addTrack(userStream.getTracks()[1], userStream);
+    rtcpConnection
+      .createOffer()
+      .then((offer) => {
+        rtcpConnection.setLocalDescription(offer);
+        socket.emit("offer", offer, roomInput.value);
+      })
+      .catch((error) => handleError(error.message, error));
+  }
+});
+
+socket.on("candidate", (candidate) => {
+  let icecandidate = new RTCIceCandidate({
+    candidate: candidate.candidate,
+    sdpMid: candidate.sdpMid,
+    sdpMLineIndex: candidate.sdpMLineIndex,
+  });
+  rtcpConnection.addIceCandidate(icecandidate);
+});
+
+socket.on("offer", (offer) => {
+  //DEBUG LOG: console.log(
+  //   `client ${socket.id} recieved forwarded offer request from server.`
+  // );
+  if (!isOwner) {
+    rtcpConnection = new RTCPeerConnection(iceServers);
+    rtcpConnection.onicecandidate = (e) => {
+      if (e.candidate) socket.emit("candidate", e.candidate, roomInput.value);
+    };
+    rtcpConnection.ontrack = onTrackFn;
+    rtcpConnection.addTrack(userStream.getTracks()[0], userStream);
+    rtcpConnection.addTrack(userStream.getTracks()[1], userStream);
+    rtcpConnection.setRemoteDescription(offer);
+    rtcpConnection.createAnswer(
+      (answer) => {
+        rtcpConnection.setLocalDescription(answer);
+        socket.emit("answer", answer, roomInput.value);
+      },
+      (error) => handleError(error.message, error)
+    );
+  }
+});
+
+socket.on("answer", (answer) => {
+  rtcpConnection.setRemoteDescription(answer);
+});
+
+const onTrackFn = (e) => {
+  peerFeed.srcObject = e.streams[0];
+  peerFeed.onloadedmetadata = () => peerFeed.play();
+};
 
 const displayError = (msg, error) => {
   if (!error) {
     error = new Error(msg);
   }
   const errorElement = document.getElementById("errors");
-  errorElement.innerHTML += `<p>${msg}</p>`;
+  const newError = document.createElement("p");
+  newError.textContent = msg;
+  errorElement.appendChild(newError);
+  setTimeout(() => {
+    errorElement.removeChild(newError);
+  }, 3000);
   if (typeof error !== "undefined") {
     console.error(error);
   }
@@ -54,33 +181,7 @@ label: ""
 __proto__: InputDeviceInfo*/
 
 document.getElementById("join-video").addEventListener("click", async () => {
-  const constraints = {};
-  const cameras = await listMediaDevice("videoinput");
-  const mics = await listMediaDevice("audioinput");
-
-  constraints.video = cameras.length !== 0 ? true : false;
-  constraints.audio = mics.length !== 0 ? true : false;
-
-  //Check for constraint video and audio true or not
-  if (constraints.video === false)
-    return displayError("Video Device doesn't exist");
-  if (constraints.audio === false)
-    return displayError("Audio Device doesn't exist");
-
-  //check user media for audio false
-  if (!(await checkUserMedia("video"))) {
-    displayError("Video Feed is broken");
-    constraints.video = false;
-    return;
-  }
-  //check user media for video false
-  if (!(await checkUserMedia("audio"))) {
-    displayError("Audio Feed is broken");
-    constraints.audio = false;
-    return;
-  }
-  //set stream to user media output
-  const stream = await navigator.mediaDevices.getUserMedia(constraints);
-  const feed = document.getElementById("video-feed");
-  feed.srcObject = stream;
+  if (roomInput.value === "") return displayError("Please enter a room name!");
+  //DEBUG LOG: console.log(`client ${socket.id} passed a join request`);
+  socket.emit("join", roomInput.value);
 });
--- src/index.js
+++ src/index.js
@@ -14,7 +14,45 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 io.on("connection", (socket) => {
-  console.log("New Connection");
+  console.log(`new connection: id=${socket.id}`);
+
+  //Join a room
+  socket.on("join", (roomName) => {
+    const rooms = io.sockets.adapter.rooms;
+    const room = rooms.get(roomName);
+
+    //isNew indicates whether a new room is being created by the user who emitted the join request.
+    const isNew = room === undefined ? true : false;
+
+    if (room === undefined || room.size === 1) {
+      socket.join(roomName);
+      socket.emit("room-joined", isNew);
+    } else {
+      socket.emit(`room-full`);
+    }
+
+    console.log(rooms);
+  });
+
+  //Indicates that current user is ready with their media and broadcasts this to the other user in the room, if available.
+  socket.on("ready", (roomName) => {
+    console.log(`server caught ready broadcasted by ${socket.id}`);
+    socket.broadcast.to(roomName).emit("ready");
+  });
+
+  socket.on("candidate", (candidate, roomName) => {
+    console.log(`New candidate request by ${socket.id}: `, candidate);
+    socket.broadcast.to(roomName).emit("candidate", candidate);
+  });
+
+  //When ice candidate is ready, the offer and answer exchange establishes the actual p2p connection between the clients.
+  socket.on("offer", (offer, roomName) => {
+    socket.broadcast.to(roomName).emit("offer", offer);
+  });
+
+  socket.on("answer", (answer, roomName) => {
+    socket.broadcast.to(roomName).emit("answer", answer);
+  });
 });
 
 const pathToPublic = path.join(__dirname, "../public");
