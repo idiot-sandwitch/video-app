44ce463dff02b6e802afd4bffad0b62ab4edc6a3
Avijit Pandey <avijeetpandey87@gmail.com>
2021-04-29 09:09:07 +0530

    changed imports to modular style

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-const express = require("express");
-const path = require("path");
-const socketio = require("socket.io");
-const http = require("http");
-require("dotenv").config();
+import express from "express";
+import path, { dirname } from "path";
+import { Server } from "socket.io";
+import http from "http";
+import { fileURLToPath } from "url";
+import { config } from "dotenv";
+config();
+
+//__dirname is declared because of modular import definitions.
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = new Server(server);
 
 io.on("connection", (socket) => {
   console.log("New Connection");
