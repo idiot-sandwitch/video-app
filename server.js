eba240d3223685d45e115fbbf99d3fe533053b2d
Avijit Pandey <avijeetpandey87@gmail.com>
2021-05-15 23:13:05 +0530

    refactored files, changed running scripts

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
 import express from "express";
-import path, { dirname } from "path";
 import { Server } from "socket.io";
 import http from "http";
-import { fileURLToPath } from "url";
 import { config } from "dotenv";
 config();
-
-//__dirname is declared because of modular import definitions.
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 io.on("connection", (socket) => {
-  console.log(`new connection: id=${socket.id}`);
+  console.log(`new connection with id: ${socket.id}`);
 
   //Join a room
   socket.on("join-room", (userId, roomName) => {
@@ -33,12 +28,10 @@ io.on("connection", (socket) => {
   });
 });
 
-const pathToPublic = path.join(__dirname, "../public");
-
 const port = process.env.PORT || 3000;
 
-app.use(express.static(pathToPublic));
+app.use(express.static("public"));
 
 server.listen(port, () => {
-  console.log(`Listening to ${port}`);
+  console.log(`HTTP server started and listening on port ${port}.`);
 });
